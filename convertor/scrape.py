8c121d1694c34735588059dbd436a2cc6e142053
chamane <solaymanech@gmail.com>
2021-07-03 20:43:09 +0000

    feed dataset

--- convertor/scrape.py
+++ convertor/scrape.py
@@ -5,7 +5,7 @@ import os
 dirname = os.path.dirname(__file__)
 output = os.path.join(dirname, '../data/twitter/arabic.json')
 
-search = 'التنموي# OR النموذج# OR #الديمقراطية_التشاركية OR #النموذج_التنموي_الجديد OR #المغرب_لي_بغينا OR #النموذج_التنموي OR #المواطن_والنموذج_التنموي OR #المجلس_الاقتصادي_والاجتماعي_والبيئي lang:ar include:nativeretweets'
+search = ' التنموي# OR النموذج# OR #الديمقراطية_التشاركية OR #النموذج_التنموي_الجديد OR #المغرب_لي_بغينا OR #النموذج_التنموي OR #المواطن_والنموذج_التنموي OR #المجلس_الاقتصادي_والاجتماعي_والبيئي OR #الرياضة المغربية OR #الرياضةـالمغربية OR #الرياضةـبالمغرب OR #الرياضة بالمغرب OR #الثقافة بالمغرب OR #الثقافةـبالمغرب OR #الثقافةـالمغربية OR #الثقافة المغربية OR #المنوعات المغربية OR #المنوعاتـالمغربية OR #المنوعات بالمغرب OR #المنوعاتـبالمغرب OR #الاقتصاد بالمغرب OR #الاقتصادـبالمغرب OR #الاقتصادـالمغربي OR #الاقتصاد المغربي OR #السياسة المغربية OR #السياسةـالمغربية OR #السياسةـبالمغرب OR #السياسة بالمغرب lang:ar include:nativeretweets'
 
 # the scraped tweets, this is a generator
 scraped_tweets = sntwitter.TwitterSearchScraper(search).get_items()
---
+++ fb_scrapper/test.py
@@ -0,0 +1,51 @@
+from facebook_scraper import get_posts
+import datetime
+import io
+
+with io.open('arabic_fb.json' , "w" , encoding='utf-8') as file:
+    file.write("[ \n")
+    for post in get_posts('CSMDMaroc'):
+        if (post['text'] is not None):
+            file.write('{"content": "'+post['text']+'" , \n "date" : "'+(str)(post['time'].strftime('%x'))+'"},\n')
+    for post in get_posts('النمودج-التنموي-الجديد-والهيئات-السياسية-أية-علاقة--109621010573656'):
+        if (post['text'] is not None):
+            file.write(
+                '{"content": "' + post['text'] + '" , \n "date" : "' + (str)(post['time'].strftime('%x')) + '"},\n')
+    for post in get_posts('ندوة-حول-موضوع-الشباب-والنموذج-التنموي-الجديد-انتظارات-ورهانات-200215-108733884030065'):
+        if (post['text'] is not None):
+            file.write(
+                '{"content": "' + post['text'] + '" , \n "date" : "' + (str)(post['time'].strftime('%x')) + '"},\n')
+    for post in get_posts('صدى-العالم-القروي-في-النموذج-التنموي-الجديد-102243404865816'):
+        if (post['text'] is not None):
+            file.write(
+                '{"content": "' + post['text'] + '" , \n "date" : "' + (str)(post['time'].strftime('%x')) + '"},\n')
+    for post in get_posts('Idees2019'):
+        if (post['text'] is not None):
+            file.write(
+                '{"content": "' + post['text'] + '" , \n "date" : "' + (str)(post['time'].strftime('%x')) + '"},\n')
+    for post in get_posts('MoDevMaroc'):
+        if (post['text'] is not None):
+            file.write(
+                '{"content": "' + post['text'] + '" , \n "date" : "' + (str)(post['time'].strftime('%x')) + '"},\n')
+    file.write("]")
+
+# for post in get_posts('النمودج-التنموي-الجديد-والهيئات-السياسية-أية-علاقة--109621010573656'):
+#     print(post['text'])
+#     cpt = cpt + 1
+#
+# for post in get_posts('ندوة-حول-موضوع-الشباب-والنموذج-التنموي-الجديد-انتظارات-ورهانات-200215-108733884030065'):
+#     print(post['text'])
+#     cpt = cpt + 1
+#
+# for post in get_posts('صدى-العالم-القروي-في-النموذج-التنموي-الجديد-102243404865816'):
+#     print(post['text'])
+#     cpt = cpt + 1
+#
+# for post in get_posts('Idees2019'):
+#     print(post['text'])
+#     cpt = cpt + 1
+#
+# for post in get_posts('MoDevMaroc'):
+#     print(post['text'])
+#     cpt = cpt + 1
+# print(cpt)
