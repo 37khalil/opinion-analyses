ff64dd952d31845328a132b754f8b12aeb460571
chamane <solaymanech@gmail.com>
2021-07-04 15:54:35 +0000

    feeding dataset

--- fb_scrapper/test.py
+++ fb_scrapper/test.py
@@ -1,5 +1,4 @@
 from facebook_scraper import get_posts
-import datetime
 import io
 
 with io.open('arabic_fb.json' , "w" , encoding='utf-8') as file:
@@ -28,24 +27,3 @@ with io.open('arabic_fb.json' , "w" , encoding='utf-8') as file:
             file.write(
                 '{"content": "' + post['text'] + '" , \n "date" : "' + (str)(post['time'].strftime('%x')) + '"},\n')
     file.write("]")
-
-# for post in get_posts('النمودج-التنموي-الجديد-والهيئات-السياسية-أية-علاقة--109621010573656'):
-#     print(post['text'])
-#     cpt = cpt + 1
-#
-# for post in get_posts('ندوة-حول-موضوع-الشباب-والنموذج-التنموي-الجديد-انتظارات-ورهانات-200215-108733884030065'):
-#     print(post['text'])
-#     cpt = cpt + 1
-#
-# for post in get_posts('صدى-العالم-القروي-في-النموذج-التنموي-الجديد-102243404865816'):
-#     print(post['text'])
-#     cpt = cpt + 1
-#
-# for post in get_posts('Idees2019'):
-#     print(post['text'])
-#     cpt = cpt + 1
-#
-# for post in get_posts('MoDevMaroc'):
-#     print(post['text'])
-#     cpt = cpt + 1
-# print(cpt)
